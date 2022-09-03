0f56b2f075a92c4dacb06cdd3e1e0d61e24131db
hugosenari <hugosenari@gmail.com>
2022-09-03 19:52:28 -0300

    feat(nimpkgs): update lock

--- source/snail.nim
+++
@@ -1,9 +0,0 @@
-##[
-  This module exports everything except `graphic.nim`.
-
-  For documentation, look at the imports
-]##
-import snail/vector, snail/matrix,snail/linsys,snail/graphic
-
-export vector,matrix,linsys
-  
